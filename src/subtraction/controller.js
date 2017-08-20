db53a4c2cd679d122bec185aa4a32d8e31e0cf88
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-21 03:12:59 +0700

    Add subtraction example content

--- src/subtraction/controller.js
+++ src/subtraction/controller.js
@@ -29,6 +29,92 @@
     $scope.$parent.navMenus[2].active = false;
     $scope.$parent.navMenus[3].href   = '.';
     $scope.$parent.navMenus[3].active = true;
+
+    // private variable
+    var userID = 100; // last user id used for addition
+    var listID = 100; // last user used for addition
+
+    $scope.title = 'Subtraction Example [Component]';
+    $scope.menus = ['Sub First', 'Sub Last', 'Sub Random'];
+    $scope.info  = 'Count';
+    $scope.users = [];
+    $scope.count = 0;
+
+    // Reset and update table user to initial state
+    function reset() {
+      userID = 100;
+      listID = 100;
+      $scope.count = 0;
+      userService.getByPage(1, function resetCallback(userList) {
+        $scope.users = userList.users;
+      });
+    }
+
+    // Get next user for subtraction
+    function getNextUser(callback) {
+      userID++;
+      listID++;
+      if (userID > 10000) {
+        userID = 1;
+      }
+      userService.getById(userID, function getNextUserCallback(user) {
+        user.id = listID;
+        callback(user);
+      });
+    }
+
+    // Add user to table at last row
+    function addUserAtLast() {
+      getNextUser(function subFirstCallback(user) {
+        $scope.users.push(user);
+      });
+    }
+
+    // Subtract first row from table user and add user to table at last row
+    function subFirst() {
+      $scope.count++;
+      $scope.users.shift();
+      addUserAtLast();
+    }
+
+    // Subtract last row from table user and add user to table at last row
+    function subLast() {
+      $scope.count++;
+      $scope.users.pop();
+      addUserAtLast();
+    }
+
+    // Subtract random row from table user and add user to table at last row
+    function subRandom() {
+      var index = Math.floor(Math.random() * 100);
+      $scope.count++;
+      $scope.users.splice(index, 1);
+      addUserAtLast();
+    }
+
+    // function for bind to child component
+    function onClickMenu(menu) {
+      switch (menu) {
+        case 'reset':
+          reset();
+          break;
+        case 'Sub First':
+          subFirst();
+          break;
+        case 'Sub Last':
+          subLast();
+          break;
+        case 'Sub Random':
+          subRandom();
+          break;
+      }
+    }
+
+    // expose this function, so it can be called from ng-click
+    $scope.onClickMenu = onClickMenu;
+
+    // Initialize page
+    reset();
   }
 
 })();
